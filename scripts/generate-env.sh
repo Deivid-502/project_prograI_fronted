84d472b0a5bb7e2a98ba27ed8658c76e66dc2fe8
David Hernandez <davidhernandez502jc@gmail.com>
2025-05-21 09:50:21 -0600

    implementacion de assets

--- scripts/generate-env.sh
+++ scripts/generate-env.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
+# Crea la carpeta assets si no existe
+mkdir -p src/assets
+
+# Genera src/assets/env.js usando la variable API_URL de Render
 cat > src/assets/env.js <<EOF
 (function (window) {
   window.__env = window.__env || {};
